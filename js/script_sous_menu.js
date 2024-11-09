f9602288e989f8b1cc6a5160874ed5dfe2f635b8
rboulkriat <rababboulkriat@outlook.fr>
2024-11-09 12:47:34 -0500

    organisation des fichiers

--- js/script_sous_menu.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('userMenu').addEventListener('click', function() {
-    var subMenu = this.querySelector('.sub-menu');
-    if (subMenu.style.display === 'block') {
-        subMenu.style.display = 'none';
-    } else {
-        subMenu.style.display = 'block';
-    }
-});
